66a9be8329d3c4c8f1d857b277e9fea6c4658c02
RGVictor99 <87677553+RGVictor99@users.noreply.github.com>
2022-06-14 11:55:49 -0300

    Add files via upload

---
+++ transportadora/src/encapsulamento/versao1/Veiculo.java
@@ -0,0 +1,14 @@
+package encapsulamento.versao1;
+
+public class Veiculo {
+	public double carga;
+	public double cargaMaxima;
+	
+	public double getCarga() {
+		return carga;
+	}
+	
+	public double getCargaMaxima() {
+		return cargaMaxima;
+	}
+}
