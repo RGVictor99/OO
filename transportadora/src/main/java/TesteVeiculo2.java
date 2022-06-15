5969aadd69d824382a88928919727ff62c49aa16
RGVictor99 <87677553+RGVictor99@users.noreply.github.com>
2022-06-14 23:11:27 -0300

    Add files via upload

---
+++ transportadora/src/main/java/TesteVeiculo2.java
@@ -0,0 +1,20 @@
+import encapsulamento.versao2.Veiculo;
+
+public class TesteVeiculo2 {
+public static void main(String[] args) {
+		
+		System.out.println("Criando um veículo com carga máxima de 10.000kg");
+		Veiculo veiculo = new Veiculo(10000.0);
+		
+	
+		System.out.println("Adicionando caixa número 1 (500kg)" + veiculo.adicionarCaixa(500.0));
+		System.out.println("Adicionando caixa número 2 (250kg)" + veiculo.adicionarCaixa(250.0));
+		System.out.println("Adicionando caixa número 3 (5000kg)" + veiculo.adicionarCaixa(5000.0));
+		System.out.println("Adicionando caixa número 4 (4000kg)" + veiculo.adicionarCaixa(4000.0));
+		System.out.println("Adicionando caixa número 5 (300kg)" + veiculo.adicionarCaixa(300.0));
+		
+		System.out.println("A carga do veículo é : " + veiculo.getCarga() + "kg");
+		
+	}
+
+}
